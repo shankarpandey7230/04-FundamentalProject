2cef87b912c27c882ac66e9d3cab861b4ae829c2
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-15 03:12:44 +1000

    refetching

--- 02-tours/starter/src/App.jsx
+++ 02-tours/starter/src/App.jsx
@@ -38,6 +38,24 @@ const App = () => {
     );
   }
   // todo
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            type="btn"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fetchTours()}
+          >
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
