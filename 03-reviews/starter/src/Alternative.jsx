046ebdb56cb2c38495fd30856d4c526cac1d95ef
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-16 01:01:58 +1000

    completion of project with css

--- 03-reviews/starter/src/Alternative.jsx
+++ 03-reviews/starter/src/Alternative.jsx
@@ -7,18 +7,29 @@ const App = () => {
   const { name, job, image, text } = people[index];
   // console.log(job);
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+
+    return number;
+  };
+
   const nextPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = (currentIndex + 1) % people.length;
+      const newIndex = currentIndex + 1;
 
-      return newIndex;
+      return checkNumber(newIndex);
     });
   };
   const prevPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = (currentIndex - 1 + people.length) % people.length;
+      const newIndex = currentIndex - 1;
 
-      return newIndex;
+      return checkNumber(newIndex);
     });
   };
   const randomPerson = () => {
@@ -26,8 +37,7 @@ const App = () => {
     if (randomNumber === index) {
       randomNumber = index + 1;
     }
-    const newIndex = randomNumber % people.length;
-    setIndex(newIndex);
+    setIndex(checkNumber(randomNumber));
   };
   return (
     <main>
