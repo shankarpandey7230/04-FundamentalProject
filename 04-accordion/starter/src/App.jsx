4ccc4dcd13df9a2e17fe0603403061f61a88e394
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-18 18:13:52 +1000

    rendering questions from local data

--- 04-accordion/starter/src/App.jsx
+++ 04-accordion/starter/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import data from "./data";
+
+import React, { useState } from "react";
+import Questions from "./Questions";
 
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
 };
 
 export default App;
