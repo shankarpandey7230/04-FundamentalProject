d70d6c6d2b28813814ddd73756086d3f45280670
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-20 16:35:21 +1000

    setting up projects

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -15,12 +15,21 @@ const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    // console.log(category);
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   // console.log(categories);
   return (
     <main>
       <section className="menu">
         <Title text="Our menu" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
