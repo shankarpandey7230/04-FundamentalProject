9763492d67d3229529c583d882f71bb8ef2658cf
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-27 11:09:33 +1000

    Setting up single items with checkbox local value

--- 10-grocery-bud/starter/src/SingleItem.jsx
+++ 10-grocery-bud/starter/src/SingleItem.jsx
@@ -1,5 +1,27 @@
-const SingleItem = () => {
-  return <h2>Single Item</h2>;
+import { useState } from "react";
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: isChecked && "line-through",
+        }}
+      >
+        {item.name}
+      </p>
+      <button className="btn remove-btn" type="button">
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default SingleItem;
