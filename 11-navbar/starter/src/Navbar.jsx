3ec2a454ff765736e554f84def131933d1d5fbdb
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-29 00:43:02 +1000

    fixed approach to display and hide the links

--- 11-navbar/starter/src/Navbar.jsx
+++ 11-navbar/starter/src/Navbar.jsx
@@ -17,21 +17,24 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                const { id, url, text } = link;
 
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+        <div
+          className={
+            showLinks ? "links-container show-container" : "links-container"
+          }
+        >
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
