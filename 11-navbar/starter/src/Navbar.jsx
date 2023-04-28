5ed5813b5eab6e1e40e2e8ded8494407ea67aa33
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-29 01:26:15 +1000

    dynamic setup for navbar

--- 11-navbar/starter/src/Navbar.jsx
+++ 11-navbar/starter/src/Navbar.jsx
@@ -1,32 +1,39 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
 
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className="logo" alt="log" />
+          <img src={logo} className="logo" alt="logo" />
           <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
 
         <div
-          className={
-            showLinks ? "links-container show-container" : "links-container"
-          }
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
         >
-          <ul className="links">
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
-
               return (
                 <li key={id}>
                   <a href={url}>{text}</a>
