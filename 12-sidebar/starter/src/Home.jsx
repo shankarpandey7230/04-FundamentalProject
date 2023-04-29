5e994c74042fdb0e205d327e647fb3c2266b2042
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-04-30 01:22:48 +1000

    styling home component

--- 12-sidebar/starter/src/Home.jsx
+++ 12-sidebar/starter/src/Home.jsx
@@ -1,9 +1,18 @@
 import { useGlobalContext } from "./context";
-
+import { FaBars } from "react-icons/fa";
 const Home = () => {
   const { openSidebar, openModal } = useGlobalContext();
-  console.log(openSidebar);
-  return <div>Home</div>;
+  // console.log(openSidebar);
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        ShowModal
+      </button>
+    </main>
+  );
 };
 
 export default Home;
