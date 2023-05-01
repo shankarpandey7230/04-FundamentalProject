e81f0aa4db9d906d40828f9a558eb7bf04cc794f
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-01 18:55:33 +1000

    Setting up Navbar and its CSS

---
+++ 13-strapi-submenus/starter/src/Navbar.jsx
@@ -0,0 +1,18 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./Context";
+const Navbar = () => {
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">Menus</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/* navlinks */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
