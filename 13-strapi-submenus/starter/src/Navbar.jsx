627fab1af0158960c4b5c8b4b891cf24752b5cee
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-02 15:38:14 +1000

    submenu showing and hiding with Styling

--- 13-strapi-submenus/starter/src/Navbar.jsx
+++ 13-strapi-submenus/starter/src/Navbar.jsx
@@ -2,9 +2,15 @@ import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./Context";
 import Navlinks from "./Navlinks";
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    // console.log(e.target);
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">Menus</h3>
         <button className="toggle-btn" onClick={openSidebar}>
--- 13-strapi-submenus/starter/src/Submenu.jsx
+++ 13-strapi-submenus/starter/src/Submenu.jsx
@@ -1,5 +1,32 @@
+import sublinks from "./data";
+import { useGlobalContext } from "./Context";
+
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  // console.log(currentPage);
+  return (
+    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Submenu;
