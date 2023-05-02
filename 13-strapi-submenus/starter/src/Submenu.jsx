1c910b7bc5c4f4366bc81bc54f5963306b3eaf13
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-02 21:23:58 +1000

    completing project

--- 13-strapi-submenus/starter/src/Submenu.jsx
+++ 13-strapi-submenus/starter/src/Submenu.jsx
@@ -1,12 +1,27 @@
-import sublinks from "./data";
 import { useGlobalContext } from "./Context";
-
+import sublinks from "./data";
+import { useRef } from "react";
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
-  // console.log(currentPage);
+
+  const submenuContainer = useRef(null);
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
   return (
-    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
@@ -28,5 +43,4 @@ const Submenu = () => {
     </div>
   );
 };
-
 export default Submenu;
