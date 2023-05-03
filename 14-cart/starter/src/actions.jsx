0248f645ac4ed554d6a2f7ad0bd47df7f2d3364e
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-03 21:18:15 +1000

    Fixing remove from actions

--- 14-cart/starter/src/actions.jsx
+++ 14-cart/starter/src/actions.jsx
@@ -1,5 +1,5 @@
 export const CLEAR_CART = "CLEAR_CART";
-export const REMOVE_CART = "REMOVE";
+export const REMOVE = "REMOVE";
 export const INCREASE = "INCREASE";
 export const DECREASE = "DECREASE";
 export const LOADING = "LOADING";
