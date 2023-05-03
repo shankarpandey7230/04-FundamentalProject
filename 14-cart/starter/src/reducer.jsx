d5b17a9c7c823594cf5379f18b7983576368c8f7
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-03 21:36:10 +1000

    clearing the cart

--- 14-cart/starter/src/reducer.jsx
+++ 14-cart/starter/src/reducer.jsx
@@ -8,6 +8,9 @@ import {
 } from "./actions";
 
 const reducer = (state, action) => {
-  return state;
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: new Map() };
+  }
+  throw new Error(`No matching action type:${action.type}`);
 };
 export default reducer;
