9c46548ab3e87728bec3e5a363cfbe3cbc98ec7f
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-03 20:32:30 +1000

    setting up the project with context.jsx

---
+++ 14-cart/starter/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
